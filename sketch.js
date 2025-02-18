38603e56d006bdc8a9c9d57995ba9469657d512e
Shah Hassan <shahhassancs@gmail.com>
2025-02-18 20:45:20 +0500

    simple circle in art

--- sketch.js
+++ sketch.js
@@ -1,29 +1,48 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
+const { lerp } = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
 
 const settings = {
-  dimensions: 'A4',
-  pixelsPerInch: 300,
-  //you can change the units to px, cm, in, pt, pc
-  units: 'cm',
+  dimensions: [2048, 2048],
 };
 
+const margin = 200;
 const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'orange';
-    context.fillRect(0, 0, width, height);
+  const createGrid = () => {
+    const points = [];
+    const count = 40;
 
-    context.beginPath();
-    context.arc(width / 2, height / 2, width * 0.2, 0, Math.PI * 2, false);
-    context.fillStyle = 'red';
-    context.fill();
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push([u, v]);
+      }
+    }
 
-    context.lineWidth = width * 0.009;
-    context.strokeStyle = 'green';
-    context.stroke();
+    return points;
   };
 
+  random.setSeed(512);
+  const points = createGrid().filter(() => random.value() > 0.5);
+  // or Math.random()
 
+  console.log("these are points returned from create Grid", points);
+  return ({ context, width, height }) => {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, width, height);
 
+    points.forEach(([u, v]) => {
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
+
+      context.beginPath();
+      context.arc(x, y, 5, 0, Math.PI * 2, false);
+      context.strokeStyle = "black";
+      context.lineWidth = "5";
+      context.stroke();
+    });
+  };
 };
 
 canvasSketch(sketch, settings);
